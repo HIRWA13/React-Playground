7571c5f968be8bc74d4b8ad57698b422be060a64
Junior Hirwa <iamhirwejr@gmail.com>
2023-09-20 14:56:29 +0200

    added the html skeleton

--- playground/src/App.jsx
+++ playground/src/App.jsx
@@ -3,7 +3,18 @@ import './App.css'
 function App() {
   return (
     <>
-     <h1>Hello everyone</h1>
+     <nav>
+        <img src ="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="react"/>
+     </nav>
+    <div>
+      <h1>Fun Facts about React</h1>
+      <ul>
+        <div></div><li>Was First released in 2013</li>
+        <div></div><li>Was originally created by Jordan Walke</li>
+        <div></div><li>is maintained by facebook</li>
+        <div></div><li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
     </>
   )
 }
