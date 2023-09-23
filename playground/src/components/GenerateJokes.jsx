e9ee99b518649d05f3b1da0922e52d75dff4b520
Junior Hirwa <iamhirwejr@gmail.com>
2023-09-23 13:48:54 +0200

    React conditional Rendering

--- playground/src/components/GenerateJokes.jsx
+++ playground/src/components/GenerateJokes.jsx
@@ -5,11 +5,11 @@ export default function GenerateJokes() {
         <>
             <article className='w-2/4 mx-auto bg-white rounded-md p-2 mt-4'>
                 <h1 className='text-2xl font-semibold'>Jokes to make you unlaugh</h1>
-                <Jokes setup={"I got my daughter a fridge for her birthday."} punchline={" I can't wait to see her face light up when she opens it."}/>
-                <Jokes setup={"How did the hacker escape the police?"} punchline={"He just ransomware!"}/>
+                <Jokes punchline={" I can't wait to see her face light up when she opens it."}/>
+                <Jokes setup={"How did the hacker escape the police?"} />
                 <Jokes setup={" Why don't pirates travel on mountain roads?"} punchline={"Scurvy."}/>
-                <Jokes setup={"Why do bees stay in the hive in the winter?"} punchline={"Swarm"}/>
-                <Jokes setup={"What's the best thing about Switzerland?"} punchline={"I don't know, but the flag is a big plus!"}/>
+                <Jokes punchline={"Swarm"}/>
+                <Jokes setup={"What's the best thing about Switzerland?"}/>
             </article>
         </>
     );
--- playground/src/components/Jokes.jsx
+++ playground/src/components/Jokes.jsx
@@ -1,9 +1,12 @@
 export default function Jokes(props) {
+    // what if some of the jokes were only punchlines without the setup?
+    // then here we could introduce the concepts of conditional rendering
+    // the conditional rendering is a concept where we use a condition inorder to render the a component
     return (
         <>
             <div className="border-2 rounded-md mt-2 p-2">
-                <p>{props.setup}</p>
-                <h1 className="text-lg font-semibold">{props.punchline}</h1>
+                {props.setup && <p>Setup:{props.setup}</p>}
+                {props.punchline && <h1 className="text-lg font-semibold"> Punchline: {props.punchline}</h1>}
             </div>
         </>
     );
