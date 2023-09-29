5c5525cb887d40423e8659b33b7afcfaa9c4951e
Junior Hirwa <iamhirwejr@gmail.com>
2023-09-29 17:07:40 +0200

    updated the accordions components

---
+++ playground/src/components/Accordions.jsx
@@ -0,0 +1,17 @@
+import QnA from "../assets/QnA";
+import Accordion from "./Accordion";
+export default function Accordions() {
+   
+  
+    const getAccordions = QnA.map(qn => {
+        return <Accordion key={qn.id} questions={qn}/>
+    })
+
+    return (
+        <>
+            <section className="w-3/4  mx-auto mt-10 grid grid-cols-3 gap-10">
+               {getAccordions}
+            </section>
+        </>
+    );
+}
