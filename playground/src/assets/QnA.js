48b6845c415fea855e43261a3483bb8b9ddc0457
Junior Hirwa <iamhirwejr@gmail.com>
2023-09-29 17:06:14 +0200

    updated the QnA component

---
+++ playground/src/assets/QnA.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        "id": 1,
+        "question": "Hi",
+        "Answer": "Hi, How are you"
+    },
+    {
+        "id": 2,
+        "question": "What is your name?",
+        "Answer": 13
+    },
+    {
+        "id": 3,
+        "question": "How old are you",
+        "Answer": 1000
+    },
+    {
+        "id": 4,
+        "question": "Where do you live?",
+        "Answer": "Sector 13"
+    },
+    {
+        "id": 5,
+        "question": "Nice to meet you",
+        "Answer": "Yeah, you too!"
+    }
+]
